d7cdf478014ca1b91a1f824f9bb9647f11649193
impfou <ilyakarachov@gmail.com>
2021-11-30 21:43:39 +0300

    Add HomeController.php

---
+++ api/src/Controller/Api/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('', name: 'home', methods: ['GET'])]
+    public function home(): Response
+    {
+        return $this->json([
+            'name' => 'JSON API',
+            'version' => '1.0'
+        ]);
+    }
+}
