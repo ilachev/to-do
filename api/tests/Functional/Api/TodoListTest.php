51d21778cb722bed6911e60b7cb7147bf7334846
impfou <ilyakarachev@gmail.com>
2021-12-04 13:59:21 +0300

    func tests

---
+++ api/tests/Functional/Api/TodoListTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Api;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class TodoListTest extends WebTestCase
+{
+    /**
+     * @throws \JsonException
+     */
+    public function testAllGet(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/api/todo-lists');
+
+        self::assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $content = $client->getResponse()->getContent();
+
+        self::assertNotFalse($content);
+        self::assertJson($content);
+
+        $data = json_decode($content, true, 512, JSON_THROW_ON_ERROR);
+
+        self::assertIsArray($data);
+        self::assertCount(2, $data);
+    }
+
+    public function testAllPost(): void
+    {
+        $client = static::createClient();
+
+        $client->request('POST', '/api/todo-lists');
+
+        self::assertEquals(405, $client->getResponse()->getStatusCode());
+    }
+}
