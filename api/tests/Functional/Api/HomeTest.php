534d359b88b95550fd81e2753009d9f680431da4
impfou <ilyakarachev@gmail.com>
2021-12-04 13:59:02 +0300

    func tests

--- api/tests/Functional/Api/HomeTest.php
+++ api/tests/Functional/Api/HomeTest.php
@@ -18,9 +18,15 @@ class HomeTest extends WebTestCase
         $client->request('GET', '/api/');
 
         self::assertEquals(200, $client->getResponse()->getStatusCode());
-        self::assertJson($content = $client->getResponse()->getContent());
+
+        $content = $client->getResponse()->getContent();
+
+        self::assertNotFalse($content);
+        self::assertJson($content);
 
         $data = json_decode($content, true, 512, JSON_THROW_ON_ERROR);
+
+        self::assertIsArray($data);
 
         self::assertEquals([
             'name' => 'JSON API',
--- api/tests/bootstrap.php
+++ api/tests/bootstrap.php
@@ -9,3 +9,36 @@ if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
+
+function bootstrap(): void
+{
+    $kernel = new \App\Kernel('test', true);
+    $kernel->boot();
+
+    $application = new \Symfony\Bundle\FrameworkBundle\Console\Application($kernel);
+    $application->setAutoExit(false);
+
+    $application->run(new \Symfony\Component\Console\Input\ArrayInput([
+        'command' => 'doctrine:database:drop',
+        '--if-exists' => '1',
+        '--force' => '1',
+    ]));
+
+    $application->run(new \Symfony\Component\Console\Input\ArrayInput([
+        'command' => 'doctrine:database:create',
+    ]));
+
+    $application->run(new \Symfony\Component\Console\Input\ArrayInput([
+        'command' => 'doctrine:schema:create',
+    ]));
+
+    $application->run(new \Symfony\Component\Console\Input\ArrayInput([
+        'command' => 'doctrine:fixtures:load',
+        '--no-interaction' => '1',
+    ]));
+
+//    $kernel->getContainer()->get('doctrine')->getConnection()->executeQuery('CREATE TABLE test (test VARCHAR(10))');
+    $kernel->shutdown();
+}
+
+bootstrap();
